a2d09c164eab69424aede6157a7ab14b8c1faa2b
Raymond Tong <raytong82@gmail.com>
2013-02-22 23:46:04 +0800

    commit in eclipse

---
+++ src/main/java/com/rym82/dummy/JavaMain.java
@@ -0,0 +1,9 @@
+package com.rym82.dummy;
+
+public class JavaMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
---
+++ src/test/java/com/rym82/dummy/JavaMainTest.java
@@ -0,0 +1,18 @@
+package com.rym82.dummy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JavaMainTest {
+
+	@Test
+	public void shouldFail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void shouldPass() {
+	}
+
+}
